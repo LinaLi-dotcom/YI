43159ca2ca90f1fd2b1a97d1f15b3896ccb300d4
lina <linasigridli@gmail.com>
2020-11-04 21:42:36 +0100

    redo att ladda upp

--- YI/ViewController/WritingViewController.swift
+++ YI/ViewController/WritingViewController.swift
@@ -10,66 +10,113 @@ import AVFoundation
 
 class Canvas: UIView{
     
-    override func draw(_ rect: CGRect) {
-//        custom drawing
-        super.draw(rect)
+    
+    var lineColor: UIColor!
+    var lineWidth: CGFloat!
+    var path: UIBezierPath!
+    var touchPoint: CGPoint!
+    var startingPoint: CGPoint!
+    
+    override func layoutSubviews() {
         
-        guard let context = UIGraphicsGetCurrentContext() else
-        {return}
+        self.clipsToBounds = true
+        self.isMultipleTouchEnabled = false
         
-//        here are my lines
-//        dummy data
-        /*
-        let startPoint = CGPoint(x: 0, y: 0)
-        let endPoint = CGPoint(x: 100, y: 100)
-        
-        context.move(to: startPoint)
-         context.addLine(to: endPoint)*/
-        
-        context.setStrokeColor(UIColor.black.cgColor)
-        context.setLineWidth(10)
-        context.setLineCap(.butt)
-        
-        lines.forEach {(line) in
-        for (i, p) in line.enumerated(){
-            if i == 0{
-                context.move(to: p)
-            } else{
-                context.addLine(to: p)
-            }
-        }
-        }
-        
-        context.strokePath()
+        lineColor = UIColor.red
+        lineWidth = 10
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first
+        startingPoint = touch?.location(in: self)
         
     }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first
+        touchPoint = touch?.location(in: self)
         
-    //    var line = [CGPoint] ()
-        var lines = [[CGPoint]] ()
+        path = UIBezierPath()
+        path.move(to: startingPoint)
+        path.addLine(to: touchPoint)
+        startingPoint = touchPoint
         
-        override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-            lines.append([CGPoint] ())
-        }
-        
-    //    track the finger as we move across screen
-        override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-            
-            guard let point = touches.first?.location(in: self) else {return}
-    //        print(point)
-          
-            guard var lastLine = lines.popLast() else {return}
-            lastLine.append(point)
-            lines.append(lastLine)
-            
-    //        var lastLine = lines.last
-    //        lastline.append(point)
-    //
-    //
-    //        line.append(point)
-            
-            setNeedsDisplay()
-        }
+        drawShapeLayer()
     }
+    
+    func drawShapeLayer (){
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.path = path.cgPath
+        shapeLayer.strokeColor = lineColor.cgColor
+        shapeLayer.lineWidth = lineWidth
+        shapeLayer.fillColor = UIColor.clear.cgColor
+        self.layer.addSublayer(shapeLayer)
+        self.setNeedsDisplay()
+    }
+    
+    func clearCanvas()
+    {
+        path.removeAllPoints()
+        self.layer.sublayers = nil
+        self.setNeedsDisplay()
+    }
+    
+    
+    //    override func draw(_ rect: CGRect) {
+////        custom drawing
+//        super.draw(rect)
+//
+//        guard let context = UIGraphicsGetCurrentContext() else
+//        {return}
+//
+//        context.setStrokeColor(UIColor.black.cgColor)
+//        context.setLineWidth(10)
+//        context.setLineCap(.butt)
+//
+//        lines.forEach {(line) in
+//        for (i, p) in line.enumerated(){
+//            if i == 0{
+//                context.move(to: p)
+//            } else{
+//                context.addLine(to: p)
+//            }
+//        }
+//        }
+//
+//        context.strokePath()
+//
+//    }
+//
+//    //    var line = [CGPoint] ()
+//        var lines = [[CGPoint]] ()
+//
+//        override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//            lines.append([CGPoint] ())
+//        }
+//
+//    //    track the finger as we move across screen
+//        override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+//
+//            guard let point = touches.first?.location(in: self) else {return}
+//    //        print(point)
+//
+//            guard var lastLine = lines.popLast() else {return}
+//            lastLine.append(point)
+//            lines.append(lastLine)
+//
+//    //        var lastLine = lines.last
+//    //        lastline.append(point)
+//    //
+//    //
+//    //        line.append(point)
+//
+//            setNeedsDisplay()
+//        }
+    @IBAction func clearButton(_ sender: Any) {
+    }
+}
+
+
 
 class WritingViewController: UIViewController {
     
@@ -125,4 +172,5 @@ class WritingViewController: UIViewController {
     @IBAction func goback(_ sender: UIButton) {
         dismiss(animated: true, completion: nil)
     }
+   
 }
